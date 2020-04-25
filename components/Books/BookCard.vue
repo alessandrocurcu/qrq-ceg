40a3ac034331a51c47435a151e226f0edd9f9f69
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-26 00:27:41 +0200

    Crea card per singolo libro per l'elenco di libri

---
+++ components/Books/BookCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <article>
+    <div class="media">
+      <aside class="media-left">
+        <img :src="book.coverImage" width="80" />
+      </aside>
+      <div class="media-content">
+        <p class="title is-5 is-spaced is-marginless">
+          <a @click="showEditModal = true">{{ book.title }}</a>
+        </p>
+        <p class="subtitle is-marginless">€ {{ book.price }}</p>
+        <div class="content is-small">
+          {{ book.author }} <br />
+          ISBN: {{ book.ISBN }}
+          <br />
+          <!-- <a @click="editBook(book)">Aggiugni al carrello</a> -->
+          <a
+            class="snipcart-add-item"
+            data-item-id="starry-night"
+            :data-item-price="book.price"
+            data-item-url="/paintings/starry-night"
+            data-item-description="libro descrizione"
+            :data-item-name="book.title"
+          >
+            Aggiugni al carrello
+          </a>
+          <!-- <span>·</span>
+                  <a @click="removeBook(index)">Delete</a> -->
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'BookCard',
+  props: {
+    book: {
+      type: Object,
+      default: () => {},
+      required: true
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <template>
   <section>
-    <b-card title="ciao" icon="user" />
+    <book-card :book="book" />
   </section>
 </template>
 
 <script>
+import BookCard from '~/components/Books/BookCard.vue'
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    BookCard
+  },
+  data() {
+    return {
+      book: {
+        id: 1,
+        ISBN: 1762167,
+        title: 'Catcher in the ray',
+        price: '23.00',
+        author: 'Salinger',
+        coverImage: 'http://lorempixel.com/400/200',
+        published: '12/01/2020'
+      }
+    }
+  }
 }
 </script>
