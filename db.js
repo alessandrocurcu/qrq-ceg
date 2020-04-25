9023ef942226829c81c12722fab1403f5434baa4
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-26 00:27:13 +0200

    Dummy data per i libri

--- db.js
+++ db.js
@@ -1,31 +1,31 @@
 const faker = require('faker')
 
 function generateData() {
-  const products = []
+  const books = []
   for (let id = 0; id < 10; id++) {
+    const ISBN = faker.random.number({ min: 1000, max: 9999 })
     const title = faker.commerce.productName()
     const price = faker.commerce.price()
-    const company = faker.company.companyName()
-    const image = faker.image.food()
+    const author = faker.name.firstName() + ' ' + faker.name.lastName()
+    const coverImage = faker.image.food()
     const date = faker.date
       .between('2018-01-01', '2018-07-31')
       .toISOString()
       .split('T')[0]
-    const fromId = faker.random.number({ min: 1000, max: 9999 })
-    const status = faker.random.number(1)
-    products.push({
+    // const fromId = faker.random.number({ min: 1000, max: 9999 })
+    books.push({
       id,
+      ISBN,
       title,
       price,
-      company,
-      image,
-      from_userId: fromId,
-      published: date,
-      status
+      author,
+      coverImage,
+      // from_userId: fromId,
+      published: date
     })
   }
 
-  return { products }
+  return { books }
 }
 
 module.exports = generateData
