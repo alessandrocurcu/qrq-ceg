93222cb3ad7946f5515c5eb942bb5c02c0c27a6f
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-26 12:33:10 +0200

    Aggiorna fake api

--- db.js
+++ db.js
@@ -8,12 +8,14 @@ function generateData() {
     const price = faker.commerce.price()
     const author = faker.name.firstName() + ' ' + faker.name.lastName()
     const slug = faker.helpers.slugify(title)
+    const briefDescription = faker.lorem.sentences(3)
+    const stock = faker.random.number({ min: 0, max: 10 })
     // const coverImage = faker.image.food()
     const date = faker.date
       .between('2018-01-01', '2018-07-31')
       .toISOString()
       .split('T')[0]
-    // const fromId = faker.random.number({ min: 1000, max: 9999 })
+    // const fromId = faker.random.number({ min: 0, max: 10 })
     books.push({
       id,
       slug,
@@ -21,6 +23,8 @@ function generateData() {
       title,
       price,
       author,
+      briefDescription,
+      stock,
       // coverImage,
       // from_userId: fromId,
       published: date
