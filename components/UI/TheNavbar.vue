473644930c18b9b53b4659b1f4aac3192193cf67
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-26 08:41:55 +0200

    Aggiorna navbar

---
+++ components/UI/TheNavbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-navbar type="is-primary">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+          alt="Lightweight UI components for Vue.js based on Bulma"
+        />
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item href="#">
+        Primo Link
+      </b-navbar-item>
+      <b-navbar-item href="#">
+        Secondo Link
+      </b-navbar-item>
+    </template>
+
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar'
+}
+</script>
+
+<style lang="scss" scoped></style>
