18e769d81996bebf2dbe6c4a9b7149c24d8ed688
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-26 22:39:21 +0200

    Aggiorna store dei libri

--- pages/index.vue
+++ pages/index.vue
@@ -7,36 +7,33 @@
 </template>
 
 <script>
-// import { mapState, mapGetters } from 'vuex'
+import { mapState } from 'vuex'
 import BookCard from '~/components/Books/BookCard.vue'
-import BookService from '~/services/BookService.js'
 
 export default {
   name: 'HomePage',
   components: {
     BookCard
   },
-  async asyncData({ error }) {
+  async fetch({ store, error }) {
     try {
-      const res = await BookService.getBooks()
-      const books = res.data.map((book) => {
-        book.coverImage = require('~/assets/img/la_tana_del_lupo.jpg')
-        return book
-      })
-
-      return {
-        books
-      }
+      await store.dispatch('books/fetchBooks')
     } catch (e) {
       error({
         statusCode: 503,
         message: 'Non sono in grado di recuperare i dati'
       })
     }
+  },
+  computed: {
+    ...mapState({
+      books: (state) =>
+        state.books.books.map((book) => {
+          book.coverImage = require('~/assets/img/la_tana_del_lupo.jpg')
+          return book
+        })
+    })
+    // ...mapGetters({ getBookById: 'books/getBookById' })
   }
-  // computed: {
-  //   ...mapState({ prova: (state) => state.books.prova }),
-  //   ...mapGetters({ getBookById: 'books/getBookById' })
-  // }
 }
 </script>
--- store/books.js
+++ store/books.js
@@ -1,8 +1,21 @@
+import BookService from '~/services/BookService.js'
 export const state = () => ({
-  prova: 'ciao',
-  categories: ['uno', 'due', 'tre', 'quattro'],
-  books: [{ id: 1 }, { id: 2 }]
+  books: []
 })
+
+export const mutations = {
+  SET_BOOKS(state, books) {
+    state.books = books
+  }
+}
+
+export const actions = {
+  fetchBooks({ commit }) {
+    return BookService.getBooks().then((res) => {
+      commit('SET_BOOKS', res.data)
+    })
+  }
+}
 
 export const getters = {
   getBookById: (state) => (id) => state.books.find((book) => book.id === id)
