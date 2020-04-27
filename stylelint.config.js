8280ab547774a2570ff1e5016956e01dded193c0
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-27 09:31:31 +0200

    Corregge una regola circola stylelint che creava conflitti

--- stylelint.config.js
+++ stylelint.config.js
@@ -19,14 +19,18 @@ module.exports = {
     // ===
     // HACK: to compensate for https://github.com/shannonmoeller/stylelint-config-prettier/issues/4
     // Modifying setting from Standard: https://github.com/stylelint/stylelint-config-standard/blob/7b76d7d0060f2e13a331806a09c2096c7536b0a6/index.js#L6
-    'at-rule-empty-line-before': [
-      'always',
-      {
-        except: ['blockless-after-same-name-blockless', 'first-nested'],
-        ignore: ['after-comment'],
-        ignoreAtRules: ['else']
-      }
-    ],
+    // 'at-rule-empty-line-before': [
+    //   'always',
+    //   {
+    //     except: [
+    //       'blockless-after-same-name-blockless',
+    //       'after-declaration',
+    //       'first-nested'
+    //     ],
+    //     ignore: ['after-comment'],
+    //     ignoreAtRules: ['else']
+    //   }
+    // ],
     // Allow SCSS and CSS module keywords beginning with `@`
     'at-rule-no-unknown': [null],
     'scss/at-rule-no-unknown': [
