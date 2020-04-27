f49842f53e68e1560a373fb790332abac51a0c07
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-27 08:41:29 +0200

    Aggiunge versione base della paginazione UI di Bulma

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,26 @@
     <div class="columns is-multiline">
       <book-card v-for="book in books" :key="book.id" :book="book" />
     </div>
+    <hr />
+    <b-pagination
+      icon-pack="fas"
+      :total="total"
+      :current.sync="current"
+      :range-before="rangeBefore"
+      :range-after="rangeAfter"
+      :order="order"
+      :size="size"
+      :simple="isSimple"
+      :rounded="isRounded"
+      :per-page="perPage"
+      icon-prev="caret-left"
+      icon-next="caret-right"
+      aria-next-label="Pagina Successiva"
+      aria-previous-label="Pagina Precedente"
+      aria-page-label="Pagina"
+      aria-current-label="Pagina Corrente"
+    >
+    </b-pagination>
   </section>
 </template>
 
@@ -26,6 +46,19 @@ export default {
         statusCode: 503,
         message: 'Non sono in grado di recuperare i dati'
       })
+    }
+  },
+  data() {
+    return {
+      total: 10,
+      current: 1,
+      perPage: 4,
+      rangeBefore: 1,
+      rangeAfter: 1,
+      order: 'is-centered',
+      size: 'is-small',
+      isSimple: false,
+      isRounded: true
     }
   },
   computed: {
