1e901d7e2c9753fee1531d1eea0e2c60b944fd24
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-27 15:45:41 +0200

    Aggiunge lodash e inserisce versione base dell'autocomplete di buefy

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,46 @@
 <template>
   <section>
+    <p class="content"><b>Selected:</b> {{ selected }}</p>
+    <b-field>
+      <b-autocomplete
+        :data="data"
+        icon-pack="fas"
+        icon="search"
+        placeholder="Cerca per titolo o autore"
+        field="title"
+        :loading="isFetching"
+        :check-infinite-scroll="true"
+        @typing="getAsyncData"
+        @select="(option) => (selected = option)"
+        @infinite-scroll="getMoreAsyncData"
+      >
+        <template slot-scope="props">
+          <div class="media">
+            <div class="media-left">
+              <img
+                width="32"
+                :src="
+                  `https://image.tmdb.org/t/p/w500/${props.option.poster_path}`
+                "
+              />
+            </div>
+            <div class="media-content">
+              {{ props.option.title }}
+              <br />
+              <small>
+                Released at {{ props.option.release_date }}, rated
+                <b>{{ props.option.vote_average }}</b>
+              </small>
+            </div>
+          </div>
+        </template>
+        <template slot="footer">
+          <span v-show="page > totalPages" class="has-text-grey">
+            Thats it! No more movies found.
+          </span>
+        </template>
+      </b-autocomplete>
+    </b-field>
     <div class="columns is-multiline">
       <book-card v-for="book in books" :key="book.id" :book="book" />
     </div>
@@ -56,6 +97,7 @@
 </template>
 
 <script>
+import debounce from 'lodash/debounce'
 import { mapState } from 'vuex'
 import BookCard from '~/components/Books/BookCard.vue'
 
@@ -85,7 +127,13 @@ export default {
       order: 'is-centered',
       size: 'is-small',
       isSimple: false,
-      isRounded: true
+      isRounded: true,
+      data: [],
+      selected: null,
+      isFetching: false,
+      name: '',
+      page: 1,
+      totalPages: 1
     }
   },
   computed: {
@@ -100,6 +148,49 @@ export default {
     })
     // ...mapGetters({ totalBooks: 'books/totalBooks' })
   },
-  watchQuery: ['page']
+  watchQuery: ['page'],
+  methods: {
+    getAsyncData: debounce(function(name) {
+      // String update
+      if (this.name !== name) {
+        this.name = name
+        this.data = []
+        this.page = 1
+        this.totalPages = 1
+      }
+      // String cleared
+      if (!name.length) {
+        this.data = []
+        this.page = 1
+        this.totalPages = 1
+        return
+      }
+      // Reached last page
+      if (this.page > this.totalPages) {
+        return
+      }
+      this.isFetching = true
+      this.$axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=bb6f51bef07465653c3e553d6ab161a8&query=${name}&page=${this.page}`
+        )
+        .then(({ data }) => {
+          console.log(data)
+          data.results.forEach((item) => this.data.push(item))
+
+          this.page++
+          this.totalPages = data.total_pages
+        })
+        .catch((error) => {
+          throw error
+        })
+        .finally(() => {
+          this.isFetching = false
+        })
+    }, 500),
+    getMoreAsyncData: debounce(function() {
+      this.getAsyncData(this.name)
+    }, 250)
+  }
 }
 </script>
