aeed326c1230f1e95a60b1697e9a2729374a4e7f
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-27 13:51:55 +0200

    Aggiorna il service dei libri con json placeholder

--- services/BookService.js
+++ services/BookService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:4000`,
+  // baseURL: `http://localhost:4000`,
+  baseURL: `https://my-json-server.typicode.com/alessandrocurcu/qrq-ceg`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
