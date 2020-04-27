04f7814e4065124a8cb0d9d75cd8800617c68587
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-27 17:03:39 +0200

    Migliora fake API data

--- db.js
+++ db.js
@@ -1,16 +1,19 @@
-const faker = require('faker')
+const faker = require('faker/locale/it')
 
 function generateData() {
   const books = []
   for (let id = 0; id < 20; id++) {
-    const ISBN = faker.random.number({ min: 1000, max: 9999 })
+    const ISBN = faker.helpers.replaceSymbolWithNumber('#############')
     const title = faker.commerce.productName()
-    const price = faker.commerce.price()
+    const price = faker.commerce.price(5, 30, 2, '€ ')
     const author = faker.name.firstName() + ' ' + faker.name.lastName()
     const slug = faker.helpers.slugify(title)
     const briefDescription = faker.lorem.sentences(3)
     const stock = faker.random.number({ min: 0, max: 10 })
-    // const coverImage = faker.image.food()
+    const coverImage = `https://picsum.photos/id/${faker.random.number({
+      min: 0,
+      max: 100
+    })}/500`
     const date = faker.date
       .between('2018-01-01', '2018-07-31')
       .toISOString()
@@ -25,7 +28,7 @@ function generateData() {
       author,
       briefDescription,
       stock,
-      // coverImage,
+      coverImage,
       // from_userId: fromId,
       published: date
     })
