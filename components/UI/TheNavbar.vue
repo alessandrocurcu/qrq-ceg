e3dd4cb50ef0cf4128723ba3b8995b697ddb350a
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-28 15:51:42 +0200

    Migliora navbar e aggiunge alcuni colori

--- components/UI/TheNavbar.vue
+++ components/UI/TheNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar wrapper-class="container">
+  <b-navbar wrapper-class="container" :spaced="true" :transparent="true">
     <template slot="brand">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
         <img
@@ -10,10 +10,16 @@
     </template>
     <template slot="start">
       <b-navbar-item tag="router-link" :to="{ path: '/libro/prova-uno' }">
-        Primo Link
+        Casa Editrice
       </b-navbar-item>
       <b-navbar-item tag="router-link" :to="{ path: '/libro/prova-due' }">
-        Secondo Link
+        Libri
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/libro/prova-due' }">
+        Autori
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/libro/prova-due' }">
+        Contatti
       </b-navbar-item>
     </template>
 
