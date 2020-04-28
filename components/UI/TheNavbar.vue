0956b7d01ff74e600f6b16cadb731eb1604805df
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-28 15:30:44 +0200

    Navbar iniziale con user e carrello

--- components/UI/TheNavbar.vue
+++ components/UI/TheNavbar.vue
@@ -18,15 +18,29 @@
     </template>
 
     <template slot="end">
-      <b-navbar-item tag="div">
-        <div class="buttons">
+      <b-navbar-item tag="router-link" to="/">
+        <b-icon
+          icon="user"
+          size="is-small"
+          style="margin-right: 0.5rem;"
+        ></b-icon>
+        login
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" to="/">
+        <b-icon
+          icon="shopping-cart"
+          size="is-small"
+          style="margin-right: 0.5rem;"
+        ></b-icon>
+        carrello
+        <!-- <div class="buttons">
           <a class="button is-primary">
             <strong>Sign up</strong>
           </a>
           <a class="button is-light">
             Log in
           </a>
-        </div>
+        </div> -->
       </b-navbar-item>
     </template>
   </b-navbar>
