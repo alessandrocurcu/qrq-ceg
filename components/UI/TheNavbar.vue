9d5b1d6134fc4efe78824b033b373b18b44bbda2
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-28 15:15:14 +0200

    Aggiunge logo

--- components/UI/TheNavbar.vue
+++ components/UI/TheNavbar.vue
@@ -3,8 +3,8 @@
     <template slot="brand">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
         <img
-          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-          alt="Lightweight UI components for Vue.js based on Bulma"
+          src="~/assets/img/logo.jpg"
+          alt="Logo di Curcu e Genovese Associati"
         />
       </b-navbar-item>
     </template>
